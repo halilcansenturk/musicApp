e0ad8b08f96153a4b7b3265fd9bba0fbf5b950e6
Halil Can Şentürk <halilcan184@hotmail.com>
2025-03-04 02:44:05 +0300

    son güncelleme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, FlatList, View } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
+import { useState } from "react";
 import music_data from "./music-data.json";
 import SongCard from "./components/SongCard";
+import SearchBar from "./components/SearchBar";
 
 export default function App() {
+  const [list, setList] = useState(music_data);
   const renderSong = ({ item }) => <SongCard song={item} />;
-
   const renderSeperator = () => <View style={styles.seperator} />;
+  //arama kısım
+  const handleSearch = (text) => {
+    const filteredList = music_data.filter((song) => {
+      const searcedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searcedText) > -1;
+    });
+
+    setList(filteredList);
+  };
   return (
     <View style={styles.container}>
+      <SearchBar onSearch={handleSearch} />
       <FlatList
         keyExtractor={(item) => item.id}
-        data={music_data}
+        data={list}
         renderItem={renderSong}
         ItemSeparatorComponent={renderSeperator} //yukarıda ve aşağıda çizgiyok
       />
